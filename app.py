2a94aff4abfc6d1b788744322ef3e0958bd99baf
HMPorl <paul.scott@thehireman.co.uk>
2025-06-18 12:49:02 +0100

    Top padding

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ st.title("Net Rates Calculator")
 st.markdown("""
     <style>
     .block-container {
-        padding-top: 3rem;
-        padding-bottom: 3rem;
+        padding-top: 5rem;
+        padding-bottom: 5rem;
     }
     .stTextInput input {
         height: 28px;
